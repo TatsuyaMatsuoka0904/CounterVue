3cdec7eb8a54952e7d5e80de8297eba043362732
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2021-12-10 14:47:54 +0900

    counter component import

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <Tutorial/>
+  <section class="container">
+      <div>
+          <Counter />
+          <hr>
+          <Counter />
+          <hr>
+          <Counter />
+          <hr>
+      </div>
+  </section>
 </template>
 
 <script>
-export default {}
+import Counter from '~/components/Counter.vue'
+
+export default {
+    components: {
+        Counter
+    }
+}
 </script>
